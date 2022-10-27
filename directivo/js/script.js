ced5a1db38b62ab466fbfd48486700ba511456da
enzo arebalos <enzoarebalos@outlook.com>
2022-10-27 08:18:30 -0300

    add habilitacion de scroll y desabilitacion cuando se selecciona un grafico

--- directivo/js/script.js
+++ directivo/js/script.js
@@ -1,18 +1,21 @@
-const fulCardBox = document.getElementById("fulCardBox");
-fulCard= document.getElementById("fulCard");
-//tomo todos los iconos
+const fullCardBox = document.getElementById("fulCardBox");
+fullCard= document.getElementById("fulCard");
+//lista de elementos de la clase icon
 icons = document.getElementsByClassName("icon");
+//elemento body
+var body = document.getElementById("page-top");
 
 function openFulCard(refId){
-    //tomo todos los iframe
+    //lista de elementos de la clase iframe
     var iframes = document.getElementsByClassName("iframe");
-    //tomo la url del iframe que necesito
-    var elem = iframes[refId].getAttribute("src");
-
-    //cargo la url
-    fulCard.src = elem;
-    //actualizo css
-    fulCardBox.style.display = "flex";
+    //elemento de la lista pasado por parametro
+    var srcIframe = iframes[refId].getAttribute("src");
+    //cargo la url en la card a mostrar
+    fullCard.src = srcIframe;
+    //muestro full card 
+    fullCardBox.style.display = "flex";
+    //quito scroll a la pagina
+    body.style.position = "fixed";
     //oculto iconos
     for (let index = 0; index < icons.length; index++) {
         icons[index].style.display="none";
@@ -22,9 +25,12 @@ function openFulCard(refId){
 }
 
 function closeCard(){
-    //actualizo css
-    fulCardBox.style.display = "none";
-    fulCard.src = "";
+    //escondo full card
+    fullCardBox.style.display = "none";
+    //saco la url de la full card
+    fullCard.src = "";
+    //habilito scroll de la pagina
+    body.style.position = "relative";
     //muestro iconos
     for (let index = 0; index < icons.length; index++) {
         icons[index].style.display="flex";
